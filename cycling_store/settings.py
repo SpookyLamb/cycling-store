504fc9071fd4337b458c519fd770ac8a571aae57
SpookyLamb <turtleytyrant@gmail.com>
2024-05-21 13:28:30 -0400

    CORS!

--- cycling_store/settings.py
+++ cycling_store/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'cycling_store_app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,22 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'cycling_store.urls'
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:8080']
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+]
+
+CORS_ALLOW_HEADERS = [
+    'Content-Type',
+    'Authorization',
+]
 
 TEMPLATES = [
     {
